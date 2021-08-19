3178ba2dd3f77544b528c5880b2bd0d136b2632e
hotruongan <89150429+hotruongan@users.noreply.github.com>
2021-08-19 22:33:09 +0700

    update

--- src/screens/PlashScreen.js
+++ src/screens/PlashScreen.js
@@ -5,7 +5,7 @@ import styles from '../styles/PlashStyle';
 const PlashScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>Hello welcome dasdastsasasa</Text>
+            <Text>Hello welcome dasdssastsasasa</Text>
         </View>
     )
 }
