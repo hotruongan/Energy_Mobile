56e7ae91854e3d3d50b5390801ea26cea0124d8d
hotruongan <hotruongan19992017@gmail.com>
2021-08-19 21:34:45 +0700

    update

---
+++ src/styles/RegisterStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const RegisterStyle = () => {
+    return (
+        <View>
+            <Text>andeptrai</Text>
+        </View>
+    )
+}
+
+export default RegisterStyle
+
+const styles = StyleSheet.create({})
